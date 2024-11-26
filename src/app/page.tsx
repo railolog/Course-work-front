f45e048ad42ceda092c11bde5dacf7d4920d1967
Daria Skvortsova <dariskvortsova@ozon.ru>
2024-11-26 17:11:11 +0300

    add empty container for fights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 "use client"
 
 import styles from "./page.module.css";
-import {useEffect, useState} from "react";
+import React, { useState } from "react";
 import {Fight, FightsMock} from "@/types/fights";
-import axios from "axios";
-import Link from "next/link";
 import Image from "next/image";
 import mainImage from '@/images/charmeleon.png';
 import secondaryImage from '@/images/absol.png';
 import { RightArrowIcon } from "@/icons/index";
 import {FightCard} from "@/components/FightCard";
+import {useAuth} from "@/context/auth";
+import {AuthModal} from "@/components/modals/AuthModal";
 
 export default function Home() {
-  const [fights, setFights] = useState<Fight[]>(FightsMock);
+    const { isAuthenticated, register } = useAuth();
+
+    const [fights, setFights] = useState<Fight[]>(FightsMock);
+    const [showModal, setShowModal] = useState<boolean>(false);
 
   // useEffect(() => {
   //     const fetchFightsAndBalance = async () => {
@@ -30,35 +33,48 @@ export default function Home() {
   // }, []);
 
   return (
-      <div className={styles.container}>
-        <div className={styles.banner}>
-          <div className={styles.textContainer}>
-            <div className={styles.title}>
-              Получи свой шанс провести бой и стать победителем
-            </div>
-            <div className={styles.subtitle}>
-              Это возможность выиграть 500 000 рублей
-            </div>
-            <button className={styles.button}>
-                Играть
-                <RightArrowIcon className={styles.icon}/>
-            </button>
-          </div>
-            <div className={styles.images}>
-                <Image className={styles.mainImage} src={mainImage} alt="Pokemons" width={undefined} height={450}/>
-                <Image className={styles.secondaryImage} src={secondaryImage} alt="Pokemons" width={undefined} height={450}/>
-            </div>
-        </div>
-        {/*<Link href="/create-fight">Создать бой</Link>*/}
-        {/*<Link href="/top-up">Пополнить баланс</Link>*/}
-          <div className={styles.fightsContainer}>
-              <div className={styles.fightsTitle}>Мои бои</div>
-              <div className={styles.fights}>
-                  {fights.map((fight) => (
-                      <FightCard fight={fight} key={fight.id}/>
-                  ))}
+      <>
+          <div className={styles.container}>
+              <div className={styles.banner}>
+                  <div className={styles.textContainer}>
+                      <div className={styles.title}>
+                          Получи свой шанс провести бой и стать <span className={styles.highlight}>победителем</span>
+                      </div>
+                      <div className={styles.subtitle}>
+                          Это возможность выиграть 500 000 рублей
+                      </div>
+                      <button className={styles.button}>
+                          Играть
+                          <RightArrowIcon className={styles.icon}/>
+                      </button>
+                  </div>
+                  <div className={styles.images}>
+                      <Image className={styles.mainImage} src={mainImage} alt="Pokemons" width={undefined}
+                             height={450}/>
+                      <Image className={styles.secondaryImage} src={secondaryImage} alt="Pokemons" width={undefined}
+                             height={450}/>
+                  </div>
               </div>
+              {/*<Link href="/create-fight">Создать бой</Link>*/}
+              {/*<Link href="/top-up">Пополнить баланс</Link>*/}
+              {isAuthenticated ? (
+                  <div className={styles.fightsContainer}>
+                      <div className={styles.fightsTitle}>Мои бои</div>
+                      <div className={styles.fights}>
+                          {fights.map((fight) => (
+                              <FightCard fight={fight} key={fight.id}/>
+                          ))}
+                      </div>
+                  </div>
+              ) : (
+                  <div className={styles.emptyContainer} onClick={() => setShowModal(true)}>
+                      <p className={styles.emptyTitle}>Начните проводить бои прямо сейчас!</p>
+                      <p className={styles.emptyText}>Здесь будут отображаться проведенные бои, зарегистрируйтесь или войти, чтобы начать</p>
+                      <button className={styles.button}>Войти</button>
+                  </div>
+              )}
           </div>
-      </div>
+          {showModal && <AuthModal onClose={() => setShowModal(false)}/>}
+      </>
   );
 }
